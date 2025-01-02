171ec5a22a6044d6034f2dfe97397fca7424dba3
garrett gerver <55259350+garrettgerver@users.noreply.github.com>
2025-01-01 22:34:12 -0500

    nvim files

---
+++ nvim/lua/config/init.lua
@@ -0,0 +1,6 @@
+require("config.remap")
+require("config.lazy")
+require("config.set")
+
+vim.cmd('hi LineNumber guibg=none ctermbg=none')
+--vim.cmd('autocmd VimEnter,VimLeave * silent !tmux set status')
